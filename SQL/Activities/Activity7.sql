935e364971d6cd75ad79176775314cf2595114af
Neha-Kri <63946951+Neha-Kri@users.noreply.github.com>
2022-01-11 16:22:08 +0530

    Add files via upload

---
+++ SQL/Activities/Activity7.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity7
+REM   Using the Aggregation Functions
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select * from orders;
+
+select SUM(purchase_amount) AS "Total sum" from orders;
+
+select * from orders;
+
